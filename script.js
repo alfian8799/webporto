9958082878d4fbeede1ac6ca3b067f3e14a3aedd
alfianza <unravel8799@gmail.com>
2023-02-06 12:24:51 +0700

    mengubah ukuran

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var burgerBar = document.querySelector('.burger');
-var listName = document.querySelector('.list-android');
+// var burgerBar = document.querySelector('.burger');
+// var listName = document.querySelector('.list-android');
 
-var open = false;
+// var open = false;
 
-burgerBar.addEventListener('click', function () {
-  open = !open;
-  if (open) {
-    listName.style.display = 'none';
-  } else {
-    listName.style.display = 'block';
+// burgerBar.addEventListener('click', function () {
+//   open = !open;
+//   if (open) {
+//     listName.style.display = 'none';
+//   } else {
+//     listName.style.display = 'block';
+//   }
+// });
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#burger').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+const menu = document.querySelector('.burger');
+
+document.addEventListener('click', function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
   }
 });
